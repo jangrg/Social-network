8162ab93675225a9bb4c7f3da8656ea49161035d
ana <ana.terovic@fer.hr>
2020-11-02 12:15:48 +0100

    fixed register

--- frontend/pages/register/index.vue
+++ frontend/pages/register/index.vue
@@ -2,7 +2,9 @@
   <div class="d-flex flex-column bg justify-content-between">
     <BrandName form="Feel free to register" />
     <div class="mx-auto">
-      <div class="lead border rounded mx-auto p-5 white-container justify-content-center">
+      <div
+        class="lead border rounded mx-auto p-5 white-container justify-content-center"
+      >
         <div class="center">
           <div>
             <strong>Register:</strong>
@@ -11,56 +13,83 @@
             <b-form-input
               v-model="form.email"
               type="email"
-              placeholder="Enter your email">
+              placeholder="Enter your email"
+            >
             </b-form-input>
 
             <b-form-input
-              v-model="form.firstname"
+              v-model="form.first_name"
               type="text"
-              placeholder="Enter your first name">
+              placeholder="Enter your first name"
+            >
             </b-form-input>
 
             <b-form-input
-              v-model="form.lastname"
+              v-model="form.last_name"
               type="text"
-              placeholder="Enter your surname">
+              placeholder="Enter your lastname"
+            >
             </b-form-input>
 
             <b-form-input
               v-model="form.username"
               type="text"
-              placeholder="Enter your username">
+              placeholder="Enter your username"
+            >
             </b-form-input>
 
             <b-form-input
               v-model="form.password"
               type="password"
               placeholder="Enter a password"
-              :class="{'notEqual': passCheck}">
+              :class="{ notEqual: passCheck }"
+            >
             </b-form-input>
 
-            <span class="passCheck" v-if="passCheck">Password should have 8 characters both letters and numbers.</span>
+            <span class="passCheck" v-if="passCheck"
+              >Password should have 8 characters both letters and numbers.</span
+            >
 
-             <b-form-input :disabled="passCheck"
+            <b-form-input
+              :disabled="passCheck"
               v-model="repeatedPassword"
               type="password"
               placeholder="Repeat the password"
-              :class="{'notEqual': passwordsDontMatch}">
+              :class="{ notEqual: passwordsDontMatch }"
+            >
             </b-form-input>
-            
+
             <span v-if="passwordsDontMatch">Passwords are not equal!</span>
 
-            <b-form-checkbox v-model="form.store" class="mt-3 mb-2">
-              Are you a store?</b-form-checkbox>
+            <b-form-input
+              v-model="form.birth_date"
+              type="date"
+              placeholder="Enter your date of birth"
+            >
+            </b-form-input>
 
-            <button @click.prevent="register" :disabled="allowSubmit" type="submit" class="btn btn-primary mt-2 text-align">
+            <b-form-checkbox v-model="form.store" class="mt-3 mb-2">
+              Are you a store?</b-form-checkbox
+            >
+
+            <button
+              @click.prevent="register"
+              :disabled="allowSubmit"
+              type="submit"
+              class="btn btn-primary mt-2 text-align"
+            >
               Submit
             </button>
 
             <p class="lead mt-4">
-            Already a user?  <b-button class="btn btn-primary text-align" variant="primary" to="/login">Login</b-button>
-          </p>
-
+              Already a user?
+              <b-button
+                class="btn btn-primary text-align"
+                variant="primary"
+                to="/login"
+                >Login</b-button
+              >
+            </p>
           </b-form>
         </div>
       </div>
@@ -70,11 +99,11 @@
 </template>
 
 <script>
-import BrandName from '../../components/BrandName'
+import BrandName from "../../components/BrandName";
 
 export default {
-  name:"RegisterForm",
-  components: {BrandName},
+  name: "RegisterForm",
+  components: { BrandName },
 
   head() {
     return {
@@ -82,61 +111,65 @@ export default {
       meta: [
         {
           name: "viewport",
-          content: "width=device-width, initial-scale=1, shrink-to-fit=no",
-        },
-      ],
+          content: "width=device-width, initial-scale=1, shrink-to-fit=no"
+        }
+      ]
     };
   },
 
   data() {
-    return{
+    return {
       form: {
-        username: '',
-        email: '',
-        firstname: '',
-        surname:'',
-        password: '',
-        store: false
+        username: "",
+        email: "",
+        first_name: "",
+        last_name: "",
+        password: "",
+        birth_date: ""
       },
-      repeatedPassword: '',
-    }
+      repeatedPassword: "",
+      store: false
+    };
   },
   computed: {
-        passwordsDontMatch() {
-        return this.form.password !== this.repeatedPassword && this.repeatedPassword.length > 0 ;
-      },
+    passwordsDontMatch() {
+      return (
+        this.form.password !== this.repeatedPassword &&
+        this.repeatedPassword.length > 0
+      );
+    },
 
-      passCheck(){
-        if (this.form.password.length >= 8  && /[a-zA-Z]/g.test(this.form.password) && /\d/.test(this.form.password))
-          return false;
+    passCheck() {
+      if (
+        this.form.password.length >= 8 &&
+        /[a-zA-Z]/g.test(this.form.password) &&
+        /\d/.test(this.form.password)
+      )
+        return false;
 
-        if (this.form.password.length > 0 )
-          return true;
-      },
+      if (this.form.password.length > 0) return true;
+    },
 
-      allowSubmit(){
-        return this.form.password !== this.repeatedPassword
-      }
+    allowSubmit() {
+      return this.form.password !== this.repeatedPassword;
+    }
   },
   methods: {
     async register() {
       try {
-        
         let response = await this.$axios.post(`account/`, this.form);
         this.$toast.show(response.data.message, { duration: 8000 });
 
         // this.$store.commit('User/SET_LOGGED_USER', response.data);
 
         // redirect to user profile
-        this.$router.push('/');
-
+        this.$router.push("/");
       } catch (e) {
         this.$toast.error(e, { duration: 8000 });
       }
-    },
+    }
   }
-}
+};
 </script>
 
-<style>
-</style>
+<style></style>
