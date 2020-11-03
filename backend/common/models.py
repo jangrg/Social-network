0d3323ccc35e3c66e7025d406d65fcbafe50758f
Jan Grgić <jan.grgic@fer.hr>
2020-11-03 14:51:54 +0000

    Update password reset URL

--- backend/common/models.py
+++ backend/common/models.py
@@ -3,15 +3,13 @@ import uuid
 from django.db import models
 from django.contrib.auth.models import AbstractUser
 from django.dispatch import receiver
-from django.urls import reverse
 from django_rest_passwordreset.signals import reset_password_token_created
 from django.core.mail import send_mail
 
 
 @receiver(reset_password_token_created)
 def password_reset_token_created(sender, instance, reset_password_token, *args, **kwargs):
-    email_plaintext_message = "{}?token={}".format(reverse('password_reset:reset-password-request'),
-                                                   reset_password_token.key)
+    email_plaintext_message = 'localhost:3000/resetpassword&token={}'.format(reset_password_token.key)
 
     send_mail(
         # title:
