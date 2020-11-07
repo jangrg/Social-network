aa3a92e599d4ba67d61f3040a8744193102e30e6
Josip Zavada <josip.zavada@fer.hr>
2020-11-07 22:21:45 +0100

    added models

--- backend/common/models.py
+++ backend/common/models.py
@@ -3,6 +3,7 @@ import uuid
 from django.db import models
 from django.contrib.auth.models import AbstractUser
 from django.dispatch import receiver
+from django.utils import timezone
 from django_rest_passwordreset.signals import reset_password_token_created
 from django.core.mail import send_mail
 
@@ -37,4 +38,71 @@ class Post(models.Model):
     type_attr = models.CharField(null=True, blank=True, max_length=255)
     likes_num = models.IntegerField(null=True, blank=True)
     time = models.DateTimeField(auto_now_add=True)
-    
+
+    page = models.ForeignKey(Page, related_name="posts", null=True)
+    user = models.ForeignKey(User, related_name="posts", null=True)
+
+
+class Category(models.Model):
+    name = models.CharField(max_length=100)
+    pages = models.ForeignKey
+
+
+class Page(models.Model):
+    #Not sure what field type this should be
+    work_time = models.CharField(null=True)
+    name = models.CharField(max_length=100)
+    date_created = models.DateTimeField(default=timezone.now())
+    location = models.CharField(max_length=100)
+    categories = models.ManyToManyField(Category, related_name="pages")
+
+    owner = models.ForeignKey(User, related_name="pages")
+
+
+class Item(models.Model):
+    size = models.IntegerField()
+    photo = models.URLField()
+    price = models.FloatField()
+    rating = models.FloatField(null=True)
+    text_content = models.TextField()
+    quantity_left = models.IntegerField()
+
+    category = models.ForeignKey(Category, related_name="items_in_category")
+    user_seller = models.ForeignKey(User, related_name="items_for_sale", null=True)
+    page_seller = models.ForeignKey(Page, related_name="items_for_sale", null=True)
+
+
+class Review(models.Model):
+    likes_num = models.IntegerField(default=0)
+    rating = models.FloatField(null=True)
+    text = models.TextField()
+    sellers_answer = models.TextField(null=True)
+
+    item = models.ForeignKey(Item, related_name="review")
+
+
+class Message(models.Model):
+    time_sent = models.DateTimeField(default=timezone.now())
+    photo = models.URLField(null=True)
+    text_content = models.TextField()
+
+    sender = models.ForeignKey(User, related_name="sent_messages")
+    receiver = models.ForeignKey(User, related_name="received_messages")
+
+
+class Notification(models.Model):
+    time_created = models.DateTimeField(default=timezone.now())
+    text_content = models.TextField()
+    marked_as_read = models.BooleanField(default=False)
+    #Not sure what field type this should be
+    color = models.CharField(null=True)
+
+    user = models.ForeignKey(User, related_name="notifications")
+
+class Comment(models.Model):
+    text = models.TextField()
+    likes_num = models.IntegerField(null=True)
+
+    post = models.ForeignKey(Post, related_name="comments")
+    user  = models.ForeignKey(User, related_name="comments")
+    parrent_comment = models.ForeignKey("replies", related_name="comment")
