60753c6d5248f9cd156026128ec5d4aa8c7e4138
Fran Jurinec <franjurinec@live.com>
2020-11-09 16:14:36 +0100

    Added dummy posts, enabled blank for post.page

--- backend/common/models.py
+++ backend/common/models.py
@@ -52,7 +52,7 @@ class Post(models.Model):
     type_attr = models.CharField(null=True, blank=True, max_length=255)
     likes_num = models.IntegerField(null=True, blank=True, default=0)
     time = models.DateTimeField(auto_now_add=True)
-    page = models.ForeignKey(Page, related_name="posts", null=True, on_delete=models.CASCADE)
+    page = models.ForeignKey(Page, related_name="posts", null=True, blank=True, on_delete=models.CASCADE)
 
 
 class Item(models.Model):
