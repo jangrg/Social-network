bb560edee1e40ba95fc089bbb9ad2f6c1d6f2ce5
Jan Grgić <jan.grgic@fer.hr>
2020-12-27 16:19:32 +0100

    Added email support

--- backend/app/settings.py
+++ backend/app/settings.py
@@ -141,7 +141,12 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
-EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_USE_TLS = True
+EMAIL_PORT = 587
+EMAIL_HOST_USER = 'weshare757@gmail.com'
+EMAIL_HOST_PASSWORD = '1Zaboraviti2'
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
